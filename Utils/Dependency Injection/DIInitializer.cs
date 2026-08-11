9e2c027c80c521034d6377f5d7bc54b245b04a50
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2026-08-11 14:17:10 +0200

    Added `ProcessScene` method to centralize dependency injection logic in `DIInitializer`. Improved initialization safety with `HashSet<IInitializable>`. Enabled execution in the Editor using `ExecuteAlways`.

--- Utils/Dependency Injection/DIInitializer.cs
+++ Utils/Dependency Injection/DIInitializer.cs
@@ -1,5 +1,6 @@
 using MLGWorks.Utils.DependencyInjection.Attributes;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -20,16 +21,50 @@ namespace MLGWorks.Utils.DependencyInjection
     /// nearly all other Awake methods, allowing all scene objects to be created and ready.
     /// </summary>
     [DefaultExecutionOrder(10000)]  // Run this *after* almost everything else
+    [ExecuteAlways]
     public class DIInitializer : MonoBehaviour
     {
+        private readonly HashSet<IInitializable> initializedObjects = new();
+
+        /// <summary>
+        /// Processes the current scene when the component becomes enabled.
+        /// <see cref="ExecuteAlwaysAttribute"/> makes this available to Editor tests,
+        /// where normal Play Mode lifecycle callbacks may not run.
+        /// </summary>
+        private void OnEnable()
+        {
+            ProcessScene();
+        }
+
         /// <summary>
         /// Called by Unity during the Awake lifecycle phase.
         /// Performs service auto-registration, dependency injection, and post-injection initialization.
         /// </summary>
         private void Awake()
         {
+            ProcessScene();
+        }
+
+        /// <summary>
+        /// Repeats the DI pass after all regular Start methods have run.
+        /// This supports services and injectable objects that are created during Awake.
+        /// </summary>
+        private void Start()
+        {
+            ProcessScene();
+        }
+
+        /// <summary>
+        /// Registers marked services, injects marked fields, and initializes each
+        /// <see cref="IInitializable"/> object once. The pass is intentionally safe to
+        /// repeat because Unity scene objects may be created after this component's Awake.
+        /// </summary>
+        private void ProcessScene()
+        {
             // Retrieve all MonoBehaviour instances in the scene, including inactive ones.
-            var monos = FindObjectsOfType<MonoBehaviour>(true);
+            var monos = FindObjectsByType<MonoBehaviour>(
+                FindObjectsInactive.Include,
+                FindObjectsSortMode.None);
 
             // Automatically register all MonoBehaviours decorated with [DIServiceAttribute].
             foreach (var mono in monos)
@@ -39,6 +74,12 @@ namespace MLGWorks.Utils.DependencyInjection
                 {
                     // Register this instance as a service using its concrete type.
                     ServiceLocator.Register(type, mono);
+
+                    // Also make the same instance available through its interfaces.
+                    foreach (var serviceInterface in type.GetInterfaces())
+                    {
+                        ServiceLocator.Register(serviceInterface, mono);
+                    }
                 }
             }
 
@@ -52,7 +93,10 @@ namespace MLGWorks.Utils.DependencyInjection
             // allowing for any additional setup after injection.
             foreach (var init in monos.OfType<IInitializable>())
             {
-                init.Initialize();
+                if (initializedObjects.Add(init))
+                {
+                    init.Initialize();
+                }
             }
         }
     }
